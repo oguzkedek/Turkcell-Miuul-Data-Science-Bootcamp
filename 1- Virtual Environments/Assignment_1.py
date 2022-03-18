ee6556cf0522c5db64db2862198eb84e10603f87
Oğuz Kedek <oguzkedek95@gmail.com>
2022-03-19 02:45:05 +0300

    Setting Up Working Environment

---
+++ 1- Virtual Environments/Assignment_1.py
@@ -0,0 +1,44 @@
+###############################################
+# ÖDEV : Kendi adınızda bir virtual environment oluşturunuz ve aşağıdakileri yapınız.
+###############################################
+
+###############################################
+# Görev 1: Kendi isminizde bir virtual environment oluşturunuz oluşturma esnasında python 3 kurulumu yapınız.
+###############################################
+
+###############################################
+# Görev 2: Oluşturduğunuz environment'ı aktif ediniz.
+###############################################
+
+###############################################
+# Görev 3: Yüklü paketleri listeleyiniz.
+###############################################
+
+###############################################
+# Görev 4: Environment içerisine Numpy'ın güncel versiyonunu ve Pandas'ın 1.2.1 versiyonunu aynı anda indiriniz.
+###############################################
+
+###############################################
+# Görev 5: İndirilen Numpy'ın versiyonu nedir?
+###############################################
+
+###############################################
+# Görev 6: Pandas'ı upgrade ediniz. Yeni versiyon nedir?
+###############################################
+
+###############################################
+# Görev 7: Numpy'ı environment'tan siliniz.
+###############################################
+
+###############################################
+# Görev 8: Seaborn kütüphanesini ve matplotlib kütüphanesini aynı anda güncel versiyonları ile indiriniz.
+###############################################
+
+###############################################
+# Görev 9: Virtual environment içindeki kütüphaneleri versiyon bilgisi ile beraber export ediniz ve yaml dosyasını inceleyiniz.
+###############################################
+
+###############################################
+# Görev 10: Oluşturduğunuz environment'i siliniz.
+###############################################
+# İpucu: Önce environment'i deactivate ediniz.
