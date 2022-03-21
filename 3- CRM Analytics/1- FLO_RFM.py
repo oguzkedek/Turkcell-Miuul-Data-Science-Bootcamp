2c6a35885c1a8fb1ca2f5ce7f92aef1db22e96e6
Oğuz Kedek <oguzkedek95@gmail.com>
2022-03-22 02:59:21 +0300

    CRM Analytics

---
+++ 3- CRM Analytics/1- FLO_RFM.py
@@ -0,0 +1,288 @@
+#######################
+# Oğuz Kedek - FLO Case - Endüstri Projesi 1
+#######################
+
+# İŞ PROBLEMİ
+
+# FLO müşterilerini segmentlere ayırıp bu segmentlere göre pazarlama stratejileri belirlemek istiyor.
+# Buna yönelik olarak müşterilerin davranışları tanımlanacak ve bu davranışlardaki öbeklenmeye göre gruplar oluşturulacak.
+
+# Veri Seti Hikayesi
+
+# master_id = Eşsizmüşterinumarası
+# order_channel = Alışveriş yapılan platforma ait hangi kanalın kullanıldığı(Android, ios, Desktop, Mobile)
+# last_order_channel = En son alışverişin yapıldığı kanal
+# first_order_date = Müşterinin yaptığı ilk alışveriş tarihi
+# last_order_date = Müşterinin yaptığı son alışveriş tarihi
+# last_order_date_online = Müşterinin online platformda yaptığı son alışveriş tarihi
+# last_order_date_offline = Müşterinin offline platformda yaptığı son alışveriş tarihi
+# order_num_total_ever_online = Müşterinin online platformda yaptığı toplam alışveriş sayısı
+# order_num_total_ever_offline = Müşterinin offline'da yaptığı toplam alışveriş sayısı
+# customer_value_total_ever_offline = Müşterinin offline alışverişlerinde ödediği toplam ücret
+# customer_value_total_ever_online = Müşterinin online alışverişlerinde ödediği toplam ücret
+# interested_in_categories_12 = Müşterinin son 12 ayda alışveriş yaptığı kategorilerin listesi
+# store_type 3 farklı companyi ifade eder.
+
+# Veri seti KVKK çerveçevesinde paylaşılamayacaktır .
+
+#%%
+
+import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+
+###############################################
+# Görev 1 Veriyi Anlama ve Hazırlama
+###############################################
+
+df_ = pd.read_csv('Hafta_3/FLO_RFM_Analizi/flo_data_20K.csv')
+df = df_.copy()
+
+df.head(10)  # İlk 10 gözlem
+
+df.columns   #Değişken isimleri
+
+df.describe(percentiles=[.01, .10, .25, .5, .75, .9, .99]).T
+
+# Betimsel İstatistik (daha fazla quartile açısından gözlemlemek daha faydalı olabilir)
+# Aykırı değerler olduğu açıkça gözlemlenmektedir fakat bu çalışma kapsamında ilgilenilmeyecektir.
+
+df.isnull().sum()
+# Boş değer analizi - Veri setinde boş değer bulunmamaktadır.
+
+df.info()
+#df.dtypes - Date değişkenlerinin veri tipi değiştirilmelidir .
+
+#%%
+
+df['omnichannel_order_num_total'] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
+df['omnichannel_customer_value_total'] = df['customer_value_total_ever_offline'] + df['customer_value_total_ever_online']
+#Her bir müşterinin toplam alışveriş sayısı ve harcaması için yeni değişkenler oluşturulmuştur.
+
+df.info()
+df.iloc[:, 3:7] = df.iloc[:, 3:7].apply(pd.to_datetime)
+# hangi sütunlarda date ifadesinin yer aldığının kolayca gözlemlenebildiği durum
+
+#cols_date = [col for col in df.columns if 'date' in col]
+#fazla veya karmaşık sıralarda sütun olduğu durum (fonksiyonlaştırmada bu yöntem kullanılacaktır)
+#df[cols_date] = df[cols_date].apply(pd.to_datetime)
+
+channel_results = df.groupby('order_channel').agg({'master_id': 'count',
+                                                   'omnichannel_order_num_total': 'mean',
+                                                   'omnichannel_customer_value_total': 'mean'})
+
+channel_results.columns = ['unique_customer', 'omnichannel_order_mean', 'omnichannel_customer_value_mean']
+
+"""
+
+- Android App kanalındaki müşteri sayısının diğer kanallara göre oldukça fazla olduğu gözlenmektedir.
+- Offline ve online harcamaların toplamını alarak oluşturduğumuz omnichannel_customer_value ortalamasına baktığımızda
+Ios App kanalı en fazla harcama ortalamasına sahip kanaldır.
+- Desktop kanalı toplam yapılan harcama ortalaması bakımından en düşük seviyedeki kanaldır.
+
+"""
+# %%
+
+df.sort_values(by='omnichannel_customer_value_total', ascending=False).head(10)
+# En fazla kazancı getiren 10 müşteri .
+
+df.sort_values(by='omnichannel_order_num_total', ascending=False).head(10)
+# En fazla siparişi veren 10 müşteri .
+
+# En fazla kazanç sağlayan müşteriler ile en fazla sipariş veren müşteriler arasında farklılıklar olduğu gözlemlenmektedir.
+# Burada alınan ürünün fiyatının da önemi bulunmaktadır. Örneğin 4 sipariş yapmış bir müşteri en fazla kazanç sağlayanlar
+# arasında 5.sırada olabilmektedir.
+
+#%%
+
+# ÇALIŞMAYI FONKSİYONLAŞTIRMA
+
+def data_prepare(df, percentiles=[.01,.10,.25,.5,.75,.9,.99]):
+    #Adım 2
+    print('\033[1m', 'İlk 10 Gözlem', '\033[0m', df.head(10), sep='\n')
+    print('-'*150)
+    print('\033[1m', 'Değişken İsimleri', '\033[0m', df.columns, sep='\n')
+    print('-' * 150)
+    print('\033[1m', 'Betimsel İstatistik', '\033[0m', df.describe(percentiles=percentiles).T, sep='\n')
+    print('-' * 150)
+    print('\033[1m', 'Boş Değer Check', '\033[0m', df.isnull().sum(), sep='\n')
+    print('-' * 150)
+    print('\033[1m', 'Değişken Tipleri İncelemesi', '\033[0m', df.dtypes, sep='\n')
+    print('-' * 150)
+
+    #Adım 3
+    df['omnichannel_order_num_total'] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
+    df['omnichannel_customer_value_total'] = df['customer_value_total_ever_offline'] + df['customer_value_total_ever_online']
+
+    #Adım 4
+    cols_date = [col for col in df.columns if "date" in col]
+    df[cols_date] = df[cols_date].apply(pd.to_datetime)
+
+    #Adım 5
+    channel_results = df.groupby('order_channel').agg({'master_id': 'count',
+                                                       'omnichannel_order_num_total': 'mean',
+                                                       'omnichannel_customer_value_total': 'mean'})
+    channel_results.columns = ['unique_customer', 'omnichannel_order_mean', 'omnichannel_customer_value_mean']
+    print('\033[1m', 'Alışveriş Kanalları Analizi', '\033[0m', channel_results, sep='\n')
+    print('-' * 150)
+
+    #Adım 6
+    print('\033[1m', 'En Fazla Kazanç Getiren 10 Müşterinin Analizi', '\033[0m',
+          df.sort_values(by='omnichannel_customer_value_total', ascending=False).head(10), sep='\n')
+    print('-' * 150)
+
+    #Adım 7
+    print('\033[1m', 'En Fazla Sipariş Veren 10 Müşterinin Analizi', '\033[0m',
+          df.sort_values(by='omnichannel_order_num_total', ascending=False).head(10), sep='\n')
+
+data_prepare(df)
+
+#%%
+
+
+###############################################################
+# Görev 2: RFM Metriklerinin Hesaplanması
+###############################################################
+
+"""
+- Recency   : Son alışverişten sonra geçen süredir .
+Recency'nin düşük olması bize müşterinin yakın zamanda alış yaptığını gösterir.
+
+- Frequency : Toplam alışveriş sayısı, sıklığıdır.
+Müşterilerin frequency değerlerinin yüksek olmasını isteriz.
+
+- Monetary  : Tüm alışverişlerde harcanan toplam paradır. İşletmeler bu değerin yüksek olmasını beklemektedir. 
+"""
+
+today_date = df['last_order_date'].max() + pd.DateOffset(days=2)
+
+df['recency'] = (today_date - df['last_order_date']).dt.days
+
+rfm = df[['recency', 'omnichannel_order_num_total', 'omnichannel_customer_value_total']]
+
+#rfm.rename(columns={'omnichannel_order_num_total': 'frequency', 'omnichannel_customer_value_total': 'monetary'}, inplace=True)
+
+rfm.columns = ['recency', 'frequency', 'monetary']
+rfm.index = df['master_id']
+
+#%%
+
+###############################################################
+# Görev 3: RF Skorunun Hesaplanması
+###############################################################
+
+rfm['recency_score'] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+rfm['frequency_score'] = pd.qcut(rfm['frequency'].rank(method='first'), 5, labels=[1, 2, 3, 4, 5])
+rfm['monetary_score'] = pd.qcut(rfm['recency'], 5, labels=[1, 2, 3, 4, 5])
+
+rfm['RF_SCORE'] = (rfm['recency_score'].astype(str) +
+                   rfm['frequency_score'].astype(str))
+
+rfm.head(10)
+
+#%%
+
+###############################################################
+# Görev 4: RF Skorunun Segment Olarak Tanımlanması
+###############################################################
+
+"""
+Not : Recency değerleri skor olarak değil ilk değeri açısından yorumlanmıştır. 
+
+- Hibernating : İnaktif durumunda olan , işletme için en faydasız konumdaki müşterilerdir. Recency değerleri en yüksek ve 
+frequency değerleri en düşük olan müşterilerdir. 
+
+- At Risk : Hibernating müşterilere göre nispeten daha sık alışveriş yapmış olmalarına karşın recency değerleri aynı şekilde yüksek 
+olan müşterileridir 
+
+- Can't loose : Frequency değerleri açısından işletmenin kaybetmemesi gereken ve üzerinde aksiyonlar alınması gereken müşterilerdir
+Son alımları eski bir tarih olmasına rağmen değerli müşterilerdir. 
+
+- About to Sleep : Recency değerleri ortalama seviyededir. Alışveriş sıklığı en alt seviyededir .
+
+- Need Attention : Ortalama recency ve frequency değerlerine sahip müşterilerdir. Dikkat edilmemesi durumunda hibernating,
+at_risk,about_to_sleep gibi segmentlerine düşmeleri muhtemeldir . Bu müşterilerin seviyesinin yukarı çekilmesi için 
+aksiyon alınması gereklidir.
+
+- Loyal_customers : İşletmenin sadık müşterileridir. Orta-yüksek recency değerine ve yüksek frequency değerlerine sahiptirler.
+
+- Promising : Yakın zamanda işletmeden alışveriş yapmış müşterilerdir fakat sıklıkları düşük seviyededir. 
+
+- New Customers : En düşük recency değerine (en yüksek recency skoru) sahip müşterilerdir . 
+
+- Potential_loyalist : Yakın zamanda alışveriş yapmış ve frequency değerleri orta - düşük seviyede olan müşterilerdir. 
+Frequency değerlerinin artması durumunda sadık müşteriler hatta champions grubuna dahi girebilirler.
+
+- Champions : Hem en yakın zamanda hem de en sık alışveriş yapan müşterilerdir. 
+
+"""
+
+seg_map = {
+    r'[1-2]{2}': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_loose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions'
+}
+
+rfm['segment'] = rfm['RF_SCORE'].replace(seg_map, regex=True)
+
+#%%
+
+rfm.loc[:, rfm.columns !='RF_SCORE'].groupby('segment').agg(['mean', 'count'])
+
+"""
+- champions ve new customers segmentlerinin yakın zamanlarda alışveriş yaptığı görülmektedir.
+- Burada ayırt edici nokta işlem sıklığıdır. Aynı zamanda champions segmentinin monetary değerleri de
+tatmin edici seviyededir.
+-can't lose segmenti yakın zamanda alışveriş yapmamasına rağmen en sık alışveriş yapan segment konumundadır
+ve bizim için değerlidir. İşletmeye en çok para kazandıran segmenttir.
+ 
+"""
+
+rfm[rfm["segment"] == "cant_loose"].head()
+
+df['interested_in_categories_12'].unique() # Unique kategorilerin incelenmesi.
+
+df_all = df.merge(rfm, how='inner', on=('master_id', 'recency'))
+#İki dataframe'de de recency sütunu bulunduğu için on parametresine recency de eklenmiştir ve suffixes durumu kurtarılmıştır.
+
+case_a = df_all.loc[((df_all['segment'] == 'champions') | (df_all['segment'] == 'loyal_customers')) &
+                    (df_all['monetary'] / df_all['frequency'] > 250) &
+                    (df_all['interested_in_categories_12'].str.contains('KADIN'))].master_id
+
+case_a.to_csv("customers.csv", index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3- CRM Analytics/2- FLO_CLTV.py
@@ -0,0 +1,178 @@
+#######################
+# Oğuz Kedek - FLO Case - Endüstri Projesi 2
+#######################
+
+
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from lifetimes.plotting import plot_period_transactions
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.4f' % x)
+from sklearn.preprocessing import MinMaxScaler
+
+# %%
+
+
+###############################################
+# Görev 1 Veriyi Hazırlama
+###############################################
+
+df_ = pd.read_csv('Hafta_3/FLO_RFM_Analizi/flo_data_20K.csv')
+df = df_.copy()
+
+df.describe().T
+df.info()
+
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = (quartile3 + 1.5 * interquantile_range).round()
+    low_limit = (quartile1 - 1.5 * interquantile_range).round()
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Bir önceki RFM probleminde keşfettiğimiz outlier değerlerini 0.01 ve 0.99'luk değerler ile değiştireceğiz.
+
+cols = ['order_num_total_ever_online', 'order_num_total_ever_offline',
+        'customer_value_total_ever_offline', 'customer_value_total_ever_online']
+
+for col in cols:
+    replace_with_thresholds(df, col)
+
+df['omnichannel_order_num_total'] = df['order_num_total_ever_online'] + df['order_num_total_ever_offline']
+df['omnichannel_customer_value_total'] = df['customer_value_total_ever_offline'] + df[
+    'customer_value_total_ever_online']
+
+df.describe().T
+df.info()
+
+df.iloc[:, 3:7] = df.iloc[:, 3:7].apply(pd.to_datetime)
+
+today_date = df['last_order_date'].max() + pd.DateOffset(days=2)
+
+###############################################
+# Görev 2 CLTV Veri Yapısının Oluşturulması
+###############################################
+
+cltv = pd.DataFrame()
+cltv['customer_id'] = df['master_id']
+cltv['recency_cltv_weekly'] = ((df['last_order_date'] - df['first_order_date']).dt.days) / 7
+cltv['T_weekly'] = ((today_date - df['first_order_date']).dt.days) / 7
+cltv['frequency'] = df['omnichannel_order_num_total']
+cltv['monetary_cltv_avg'] = df['omnichannel_customer_value_total'] / cltv['frequency']
+cltv = cltv[cltv['frequency'] > 1]
+
+cltv.describe().T
+
+
+#%%
+
+#################################################################################
+# Görev 3 BG/NBD, Gamma-Gamma Modellerinin Kurulması ve CLTV’ninHesaplanması
+#################################################################################
+
+
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+
+bgf.fit(cltv['frequency'],
+        cltv['recency_cltv_weekly'],
+        cltv['T_weekly'])
+
+cltv["expected_sales_3_month"] = bgf.predict(12,
+                                             cltv['frequency'],
+                                             cltv['recency_cltv_weekly'],
+                                             cltv['T_weekly'])
+
+cltv["expected_sales_6_month"] = bgf.predict(24,
+                                             cltv['frequency'],
+                                             cltv['recency_cltv_weekly'],
+                                             cltv['T_weekly'])
+
+cltv.sort_values(by='expected_sales_3_month', ascending=False)[['customer_id', 'expected_sales_3_month']].head(10)
+cltv.sort_values(by='expected_sales_6_month', ascending=False)[['customer_id', 'expected_sales_6_month']].head(10)
+
+# 3 aylık ve 6 aylık periyotlarda en fazla satın alım yapması beklenen 10 müşteri aynı müşterilerdir.
+
+cltv.describe().T
+
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+ggf.fit(cltv['frequency'], cltv['monetary_cltv_avg'])
+
+cltv['exp_average_value'] = ggf.conditional_expected_average_profit(cltv['frequency'],
+                                                                    cltv['monetary_cltv_avg'])
+
+cltv['cltv'] = ggf.customer_lifetime_value(bgf,
+                                           cltv['frequency'],
+                                           cltv['recency_cltv_weekly'],
+                                           cltv['T_weekly'],
+                                           cltv["monetary_cltv_avg"],
+                                           time=6,
+                                           freq='W',
+                                           discount_rate=0.01)
+
+cltv.head()
+
+#%%
+
+#######################################################
+# Görev 4  CLTV Değerine Göre SegmentlerinOluşturulması
+#######################################################
+
+scaler = MinMaxScaler()
+cltv['scaled_cltv'] = scaler.fit_transform(cltv[['cltv']])
+
+cltv.sort_values(by='cltv', ascending=False).head(20)
+
+"""
+
+En fazla satın alım yapması beklenen müşteriler ile cltv değeri en yüksek müşteriler arasında farklılıkLar bulunmaktadır. 
+Cltv değerlendirmesinde tek başına satın alım sayısının etkili olmadığı görülmektedir.
+
+"""
+
+cltv['segment'] = pd.qcut(cltv['scaled_cltv'], 4, labels=['S4', 'S3', 'S2', 'S1'])
+
+cltv.groupby('segment').agg({'std', 'mean', 'sum'})
+
+#%%
+
+
+"""
+Yorumlar
+ 
+- Adım 2 
+
+Segmentlerin standart sapma , ortalama , toplam gibi metrikler açısından değerlendirilmesinde 
+segmentler birbirinden belirgin şekilde ayrılmaktadır. Segmentlerin kendi içinde değerlendirilmesinde 
+standart sapmaların yüksek olduğu da gözlemlenmektedir. Özellikle S1 segmentinin standart sapması yüksektir 
+ve bu segmenti parçalayabiliriz. KMeans ile küme içi benzerlikler maksimum ve kümeler arası benzerlikler
+minimum olacak şekilde farklı bir kümeleme,segmentasyon işlemi de yapılabilir.
+
+- Adım 3 
+
+ S1 : S1 segmenti en yüksek cltv skoruna sahip , bizim için oldukça değerli müşterilerdir. Aynı zamanda bu müşterileri
+ elde tutmak yeni müşteri kazanmaktan daha az maliyetlidir. Bu müşterilerle özel ilgilenilmesi gerekmektedir
+. Sadakatin devamlılığını sağlamak için sosyal etkileşimde kalınabilir. Aynı zamanda müşteri ne kadar harcarsa o kadar 
+kazanacağı şekilde bir sadakat planı geliştirebilir ve bu sayede müşterilerin devamlılığı korunabilir. Özel indirimler 
+yapılabilir fakat bu konuda dikkatli olmak gerekmektedir. Çünkü bu negatif etki de edebilir. Planlama etkin şekilde 
+yapılmalıdır. 
+
+ S3 : Henüz kaybetmediğimiz fakat doğru stratejilerle kendimize bağlayabileceğimiz müşterilerdir. S4 segmentini 
+tekrar kazanmaktan daha olası ve daha az maliyetlidir.Son siparişleri üzerinden geçen süre belirli bir filtreye 
+sokulabilir ve filtre sonucundaki müşterilerle ilgilenilebilir. Sınırlı süreli indirimler, çapraz satış önerileri , 
+yeni teklifler sunulabilir. 
+
+"""
